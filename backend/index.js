ec7f0a4ba1dcfc8bc35f90eaf1c3e8bb63c269e2
Abdullah Sozer <aptullahsozer@gmail.com>
2026-09-08 14:31:19 +0300

    Fix Express wildcard route definitely using app.use

--- backend/index.js
+++ backend/index.js
@@ -537,7 +537,7 @@ const frontendPath = path.join(__dirname, '../frontend/dist');
 app.use(express.static(frontendPath));
 
 // API olmayan tüm istekleri React'a (index.html) yönlendir
-app.get('/:path(.*)', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
